39cd08aee460019ee4684699f3d21112fe364d75
madhu-wepkorps <madhu@webkorps.com>
2024-01-18 16:04:26 +0530

    update routes and add test cases

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  get 'apply', to: 'users#apply'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index, :show] do
+    collection do
+      get :apply
+    end
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,12 +2,13 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get users_index_url
+    get users_url
     assert_response :success
   end
 
   test "should get show" do
-    get users_show_url
+    # Assuming you have a user with id 1 in your test database
+    get user_url(users(:one))
     assert_response :success
   end
 end
